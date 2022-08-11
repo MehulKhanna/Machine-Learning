8a682b30cd88a220b7b74aea88f28acf51754568
MehulKhanna <mehulkhanna420@gmail.com>
2022-08-11 14:02:21 +0530

    Added Project squares
    Predict square of a number using Polynomial Regression

---
+++ Workstation/tools/curved_line.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.interpolate import make_interp_spline
+
+def interpolate(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    x_new = np.linspace(x.min(), x.max(), 100)
+    spl = make_interp_spline(x, y, k=3)
+    y_smooth = spl(x_new)
+
+    return x_new, y_smooth
