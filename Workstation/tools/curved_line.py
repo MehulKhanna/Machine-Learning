1600a3b6ab242dba2d4cea230050597cc4872fd3
MehulKhanna <mehulkhanna420@gmail.com>
2022-08-16 17:01:46 +0530

    Test

--- Workstation/tools/curved_line.py
+++ Workstation/tools/curved_line.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.interpolate import make_interp_spline
 
+
 def interpolate(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     x_new = np.linspace(x.min(), x.max(), 100)
     spl = make_interp_spline(x, y, k=3)
